7c019c4d0ecb2b8b3fe19dcfa84ca8eed0d1892f
Roop Majumder <roopmajumder5@gmail.com>
2024-02-10 22:51:40 +0530

    commit

--- src/components/_Trending.jsx
+++ src/components/_Trending.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { host } from '../configs/api'
+import { mostTrending } from '../configs/trending'
 
 const Trending = () => {
-  const [trendingData, setTrendingData] = useState([])
+  // const [trendingData, setTrendingData] = useState([])
 
-  useEffect(() => {
-    const getTrendingData = async () => {
-      let api = `${host}/playlists?id=1179495885`
-      let response = await fetch(api)
-      let data = await response.json()
-      setTrendingData(data.data.songs)
-    }
-    getTrendingData()
-  }, [])
+  // useEffect(() => {
+  //   const getTrendingData = async () => {
+  //     let api = `${host}/playlists?id=1179495885`
+  //     let response = await fetch(api)
+  //     let data = await response.json()
+  //     setTrendingData(data.data.songs)
+  //   }
+  //   getTrendingData()
+  // }, [])
   return (
     <div className="trendingsPage">
       <div className="trendingCont">
-            {trendingData?trendingData?.map((song, key) => {
+            {mostTrending?mostTrending?.map((song, key) => {
                 if(key > 4) return;
                 return <Link to={"/id/"+song.id} style={{textDecoration: "none", color: "black"}} key={key}>
                     <div className="trendingSongDiv">
-                        <img src={song?.image[song?.image?.length - 1]?.link} alt="" className="img" width={50}/>
+                        <img src={song?.cover} alt="" className="img" width={50}/>
                         <div className="info">
-                            <h3 dangerouslySetInnerHTML={{__html: song?.name}}></h3>
-                            <h4 dangerouslySetInnerHTML={{__html: song?.primaryArtists}}></h4>
+                            <h3 dangerouslySetInnerHTML={{__html: song?.title}}></h3>
+                            <h4 dangerouslySetInnerHTML={{__html: song?.artists}}></h4>
                         </div>
                     </div>
                 </Link>
---
+++ src/configs/trending.js
@@ -0,0 +1,32 @@
+export const mostTrending = [
+    {
+        id: "wcsDiSsA",
+        cover: "https://c.saavncdn.com/139/Dunki-Hindi-2023-20231220211003-500x500.jpg",
+        artists: "Pritam, Arijit Singh, Irshad Kamil",
+        title: "O Maahi"
+    },
+    {
+        id: "UJ0dGLpm",
+        cover: "https://c.saavncdn.com/652/Dabangg-2-Hindi-2012-20221213012606-500x500.jpg",
+        artists: "Rahat Fateh Ali Khan, Shreya Ghoshal",
+        title: "Dagabaaz Re"
+    },
+    {
+        id: "KFcbby8R",
+        cover: "https://c.saavncdn.com/238/Shershaah-Original-Motion-Picture-Soundtrack--Hindi-2021-20210815181610-500x500.jpg",
+        artists: "Jasleen Royal, B Praak, Romy, Anvita Dutt Guptan",
+        title: "Ranjha"
+    },
+    {
+        id: "6o8JoQ8b",
+        cover: "https://c.saavncdn.com/286/WMG_190295851286-English-2017-500x500.jpg",
+        artists: "Ed Sheeran",
+        title: "Perfect"
+    },
+    {
+        id: "JJDnpfBX",
+        cover: "https://c.saavncdn.com/825/My-Name-Is-Khan-Hindi-2010-20210528120940-500x500.jpg",
+        artists: "Shankar-Ehsaan-Loy, Shafqat Amanat Ali",
+        title: "Tere Naina"
+    },
+]
